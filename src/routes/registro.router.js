bdbc58995c804e016fcc3ab7e769ea84a7223e58
gianfranco cherubini <cherussa@gmail.com>
2023-12-28 17:08:05 -0300

    lsito

--- src/routes/registro.router.js
+++ src/routes/registro.router.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const usuariosModelo = require('../dao/models/usuarios.model')
+const usuariosModelo = require('../dao/models/usuarios.model');
 const crypto = require('crypto');
 const router = Router();
 
@@ -12,43 +12,32 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
     let { nombre, email, password } = req.body;
-
     if (!nombre || !email || !password) {
-        res.status(400).redirect('/api/registro?errorMessage=Complete todos los datos');
-        return;
+        return res.status(400).redirect('/api/registro?errorMessage=Complete todos los datos');
     }
 
     let regMail = /^(([^<>()\[\]\\.,;:\s@”]+(\.[^<>()\[\]\\.,;:\s@”]+)*)|(“.+”))@((\[[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}])|(([a-zA-Z\-0–9]+\.)+[a-zA-Z]{2,}))$/;
-
     if (!regMail.test(email)) {
-        res.status(400).redirect('/api/registro?errorMessage=Mail con formato incorrecto...!!!');
-        return;
+        return res.status(400).redirect('/api/registro?errorMessage=Mail con formato incorrecto...!!!');
     }
 
     let existe = await usuariosModelo.findOne({ email });
-
     if (existe) {
-        res.status(400).redirect(`/api/registro?errorMessage=Existen usuarios con email ${email} en la BD`);
-        return;
+        return res.status(400).redirect(`/api/registro?errorMessage=Existen usuarios con email ${email} en la BD`);
     }
 
     let rol = 'usuario';
-
     if (email === 'adminCoder@coder.com' && password === 'coder123') {
         rol = 'administrador';
     }
 
     password = crypto.createHmac("sha256", "codercoder123").update(password).digest("hex");
-
+    let usuario;
     try {
-        let usuario = await usuariosModelo.create({ nombre, email, password, rol });
-        res.setHeader('Content-Type', 'application/json');
-        res.status(201).json({ success: true, message: 'Usuario creado correctamente', usuario });
-        res.redirect(`/api/login?message=Usuario ${email} registrado correctamente`);
+        usuario = await usuariosModelo.create({ nombre, email, password, rol });
+        return res.redirect(`/api/login?message=Usuario ${email} registrado correctamente`);
     } catch (error) {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(500).json({ error: 'Error inesperado' });
-        res.redirect('/api/registro?error=Error inesperado. Reintente en unos minutos');
+        return res.status(500).redirect('/api/registro?error=Error inesperado. Reintente en unos minutos');
     }
 });
 
